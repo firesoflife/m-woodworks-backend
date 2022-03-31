8efbeaa8a3b4fb9d311f7a3de4943afc396939b3
firesoflife <bryangoertz@gmail.com>
2022-03-31 19:01:27 -0400

    initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+  skip_before_action :authenticate_request, only: [:create]
+  before_action :set_user, only: %i[show destroy]
+
+  # GET /users
+
+  def index
+    @users = User.all
+    render json: @users, status: :ok
+  end
+
+  # GET /users/{username}
+  def show
+    render json: @user, status: :ok
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      rneder json: @user, status: :created
+    else
+      render json: { errors: @user.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
+
+  # PUT /users/{username}
+  def update
+    unless @user.update(user_params)
+      render json: { errors: @user.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/{username}
+  def destroy
+    @user.destroy
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+end
