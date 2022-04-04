215a13aeabe3e2e9e597b2b91ca94ba5ac96640d
firesoflife <bryangoertz@gmail.com>
2022-04-03 20:53:56 -0400

    galleries for projects set up

--- db/migrate/20220404004734_create_projects.rb
+++ db/migrate/20220404004734_create_projects.rb
@@ -2,9 +2,10 @@ class CreateProjects < ActiveRecord::Migration[7.0]
   def change
     create_table :projects do |t|
       t.string :project_name
-      t.string :type
-      t.string :tags
-      t.string :gallery
+      t.string :project_type
+      t.string :project_tags
+      t.string :description
+
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_31_221545) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_04_004734) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "galleries", force: :cascade do |t|
+    t.string "image_url"
+    t.string "cover_image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "project_name"
+    t.string "project_type"
+    t.string "project_tags"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
